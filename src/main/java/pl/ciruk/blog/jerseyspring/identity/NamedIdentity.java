60f9a17168e7d4eaf26fa171241da31dfc1f5d91
c.piotre <c.piotre@gmail.com>
2015-10-12 00:46:00 +0200

    Make @Named bean act as @Component in Jersey context

--- src/main/java/pl/ciruk/blog/jerseyspring/identity/NamedIdentity.java
+++ src/main/java/pl/ciruk/blog/jerseyspring/identity/NamedIdentity.java
@@ -2,26 +2,41 @@ package pl.ciruk.blog.jerseyspring.identity;
 
 import org.slf4j.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
 
 @Path(NamedIdentity.PATH)
 @Named
+@Singleton
 public class NamedIdentity {
 
 	public static final String PATH = "/identity/named";
+
+	public static NamedIdentity SELF;
 
 	private List<Logger> loggers;
 
 	@Inject
 	public NamedIdentity(List<Logger> loggers) {
-		this.loggers = loggers;
+		this.loggers = Optional.ofNullable(SELF)
+				.map(ni -> ni.loggers)
+				.orElse(loggers);
+	}
+
+	@PostConstruct
+	void keepFirstInstance() {
+		if (SELF == null) {
+			SELF = this;
+		}
 	}
 
 	@GET
